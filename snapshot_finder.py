4d46bf80c606363f854eaadc7675fcb1a05db8e5
Doug Neal <doug@neal.me.uk>
2018-08-08 12:35:04 +0100

    Emit log messages from RdsSnapshotFinder

--- snapshot_finder.py
+++ snapshot_finder.py
@@ -1,6 +1,9 @@
 import boto3
 import dns.resolver
 import dns.name
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class RdsSnapshotFinder:
@@ -22,14 +25,26 @@ class RdsSnapshotFinder:
         self.snapshot_identifier = snapshot_identifier
         self.rds_client = boto3.client('rds')
 
+        logger.info("Initialised RDS Snapshot Finder")
+
     def get_snapshot_identifier(self):
         if self.snapshot_identifier is None:
+            logger.info("Discovering snapshot identifier...")
+
+            source_instance_id = self.get_source_instance_identifier()
             response = self.rds_client.describe_db_snapshots(
-                DBInstanceIdentifier=self.get_source_instance_identifier()
+                DBInstanceIdentifier=source_instance_id,
             )
+            # TODO check for error
 
             if len(response['DBSnapshots']) == 0:
                 raise Exception("No snapshots found")
+
+            logger.debug(
+                "Found %d snapshots for %s",
+                len(response['DBSnapshots']),
+                source_instance_id,
+            )
 
             response['DBSnapshots'].sort(
                 key=lambda x: x['SnapshotCreateTime'],
@@ -38,22 +53,43 @@ class RdsSnapshotFinder:
             most_recent = response['DBSnapshots'][0]
             self.snapshot_identifier = most_recent['DBSnapshotIdentifier']
 
+            logger.info("Using snapshot %s", self.snapshot_identifier)
+
         return self.snapshot_identifier
 
     def get_source_instance_identifier(self):
         if self.source_instance_identifier is None:
+            logger.info("Discovering source RDS instance identifier...")
+
             i = self.get_source_instance()
             self.source_instance_identifier = i['DBInstanceIdentifier']
+
+            logger.info("Using source RDS instance %s", self.source_instance_identifier)
 
         return self.source_instance_identifier
 
     def get_source_instance(self):
         if self.source_instance is None:
             if self.source_instance_identifier is None:
+                logger.info("Discovering source RDS instance...")
+
                 rds_instances = self.rds_client.describe_db_instances()
+                # TODO: check error
+
+                logger.debug(
+                    "Enumerated %d RDS instances",
+                    len(rds_instances['DBInstances'])
+                )
 
                 for instance in rds_instances['DBInstances']:
                     if instance['Endpoint']['Address'] == self.get_rds_endpoint_address():
+                        logger.info(
+                            "RDS instance %s matches endpoint address %s:%s",
+                            instance['DBInstanceIdentifier'],
+                            instance['Endpoint']['Address'],
+                            instance['Endpoint']['Port'],
+                        )
+
                         self.source_instance = instance
                         self.source_instance_identifier = instance['DBInstanceIdentifier']
                         break
@@ -61,6 +97,7 @@ class RdsSnapshotFinder:
                 if self.source_instance is None:
                     raise Exception("TODO: error text")
             else:
+                logger.info("Looking up RDS instance %s ...", self.source_instance_identifier)
                 rds_instances = self.rds_client.describe_db_instances(
                     DBInstanceIdentifier=self.source_instance_identifier
                 )
@@ -73,12 +110,19 @@ class RdsSnapshotFinder:
 
     def get_hostname(self):
         if self.hostname is None:
+            logger.info(
+                "Using hostname default for %s: '%s'",
+                self.dbms,
+                self.hostname_defaults[self.dbms],
+            )
             self.hostname = self.hostname_defaults[self.dbms]
 
         return self.hostname
 
     def get_rds_endpoint_address(self):
         if self.rds_endpoint_address is None:
+            logger.info("Discovering RDS endpoint address via DNS...")
+
             try:
                 resolver = dns.resolver.Resolver()
                 resolution = resolver.query(self.get_hostname())
@@ -90,5 +134,10 @@ class RdsSnapshotFinder:
                 raise Exception("TODO: error text 2")
 
             self.rds_endpoint_address = cname.to_text().rstrip('.')
+            logger.info(
+                "Resolved RDS endpoint address of %s to %s",
+                self.get_hostname(),
+                self.rds_endpoint_address,
+            )
 
         return self.rds_endpoint_address
