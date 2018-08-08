c2bcde8a572ec0c3e0cbbe83f2977b6c0948c452
Doug Neal <doug@neal.me.uk>
2018-08-08 12:09:47 +0100

    Emit log messages from ScrubWorkspaceInstance

--- scrub_workspace_instance.py
+++ scrub_workspace_instance.py
@@ -1,17 +1,26 @@
 import boto3
 import random
 import time
+import logging
 from datetime import datetime
+
+logger = logging.getLogger(__name__)
 
 
 class ScrubWorkspaceInstance:
-    def __init__(self, snapshot_finder):
+    def __init__(self, snapshot_finder, timeout=90):
         self.rds_client = boto3.client('rds')
         self.snapshot_finder = snapshot_finder
+        self.timeout = timeout
         self.instance_identifier = "scrubber-workspace-{0}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
         self.password = "{0:x}".format(random.getrandbits(64 * 4))
         self.instance = None
         self.source_instance = self.snapshot_finder.get_source_instance()
+
+        logger.info(
+            "Initialised scrub workspace instance, DBInstanceIdentifier: %s",
+            self.instance_identifier
+        )
 
     def get_endpoint(self):
         i = self.get_instance()
@@ -19,6 +28,10 @@ class ScrubWorkspaceInstance:
 
     def get_instance(self):
         if self.instance is None:
+            logger.info(
+                "Instance %s doesn't exist yet, creating",
+                self.instance_identifier
+            )
             self.__create_instance()
 
         return self.instance
@@ -26,6 +39,7 @@ class ScrubWorkspaceInstance:
     def cleanup(self):
         if self.instance is not None:
             rds = self.rds_client
+            logger.info("Deleting RDS instance %s", self.instance_identifier)
             rds.delete_db_instance(
                 DBInstanceIdentifier=self.instance_identifier(),
                 FinalDBSnapshotIdentifier=self.final_snapshot_identifier(),
@@ -37,24 +51,41 @@ class ScrubWorkspaceInstance:
 
     def __create_instance(self):
         rds = self.rds_client
+        source_snapshot_id = self.snapshot_finder.get_snapshot_identifier()
+        subnet_group_name = self.source_instance['DBSubnetGroup']['DBSubnetGroupName']
+
+        logger.info(
+            "Restoring instance %s from snapshot %s in subnet group %s. Timeout: %s minutes",
+            self.instance_identifier,
+            source_snapshot_id,
+            subnet_group_name,
+            self.timeout,
+        )
 
         restore_response = rds.restore_db_instance_from_db_snapshot(
             DBInstanceIdentifier=self.instance_identifier,
-            DBSnapshotIdentifier=self.snapshot_finder.get_snapshot_identifier(),
-            DBSubnetGroupName=self.source_instance['DBSubnetGroup']['DBSubnetGroupName'],
+            DBSnapshotIdentifier=source_snapshot_id,
+            DBSubnetGroupName=subnet_group_name,
         )
         # TODO check for error
 
-        for i in range(120):
+        max_end_time = time.time() + 60 * self.timeout
+        while time.time() <= max_end_time:
             poll_response = rds.describe_db_instances(
                 DBInstanceIdentifier=self.instance_identifier
             )
             # TODO check for error
             self.instance = poll_response['DBInstances'][0]
-            print("Waiting for instance to become available, current status == '{0}'".format(self.instance['DBInstanceStatus']))
+
+            logger.info(
+                "Waiting for %s to become available, current status: '%s'",
+                self.instance_identifier,
+                self.instance['DBInstanceStatus'],
+            )
+
             if self.instance['DBInstanceStatus'] == 'available':
                 return
             else:
-                time.sleep(2)
+                time.sleep(5)
 
-        raise Exception("Timeout creating instance, instance status: '{0}'".format(self.instance['DBInstanceStatus']))
+        raise Exception("Timeout creating instance")
