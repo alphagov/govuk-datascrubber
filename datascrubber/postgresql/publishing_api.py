30e9bc155cf7576209990e02943332b92274ce77
Doug Neal <doug@neal.me.uk>
2018-08-15 17:54:47 +0100

    Add scrub functions and SQL scripts for Postgres databases

---
+++ datascrubber/postgresql/publishing_api.py
@@ -0,0 +1,14 @@
+import logging
+import os.path
+
+
+def scrub_publishing_api(cursor):
+    logger = logging.getLogger('scrub_publishing_api')
+    sql_script = os.path.join(
+        os.path.dirname(__file__),
+        'scrub_publishing_api.sql'
+    )
+    logger.info("Loading SQL from %s", sql_script)
+    sql = open(sql_script, 'r').read()
+    logger.info("Executing SQL (%d lines) ...", len(sql.split('\n')))
+    cursor.execute(sql)
