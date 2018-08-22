ed788e5a4355958f50a094b86ae82b1c67d07920
Doug Neal <doug@neal.me.uk>
2018-08-22 17:13:02 +0100

    Add setup.py
    So that pip understands that this is a package...

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="datascrubber",
+    version="0.1",
+    author="GOV.UK Reliability Engineering",
+    author_email="reliability-engineering@digital.cabinet-office.gov.uk",
+    description="Scrubs sensitive data from databases",
+    url="https://github.com/alphagov/govuk-datascrubber",
+    packages=setuptools.find_packages(),
+    package_data={'datascrubber': ['sql/*.sql']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
